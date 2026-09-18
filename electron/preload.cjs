505f9669856ccd4c47fb7ce42943e47aa60201f1
Decall520 <1964146809@qq.com>
2026-09-18 17:27:05 +0800

    Add electron/preload.cjs

---
+++ electron/preload.cjs
@@ -0,0 +1,16 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("clutchbook", {
+  capture: {
+    listSources: () => ipcRenderer.invoke("capture:list-sources"),
+    selectSource: (sourceId) => ipcRenderer.invoke("capture:select-source", sourceId)
+  },
+  recording: {
+    save: (bytes, extension) =>
+      ipcRenderer.invoke("recording:save", { bytes, extension })
+  },
+  system: {
+    info: () => ipcRenderer.invoke("system:info"),
+    openPath: (targetPath) => ipcRenderer.invoke("system:open-path", targetPath)
+  }
+});
